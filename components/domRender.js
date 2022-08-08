f165ca64fea4db62369ad8f0d5d7f2cc404e1099
Matt <matthew.t.kissinger@gmail.com>
2022-08-08 16:27:27 -0700

    main UI has been styled

--- components/domRender.js
+++ components/domRender.js
@@ -1,8 +1,7 @@
 // render the two gameboards and ship icons
 
 const renderDOM = () => {
-    // create playerboard
-    // create compboard
+ 
     console.log('rendering the dom');
 
     // cache DOM
@@ -11,8 +10,13 @@ const renderDOM = () => {
     let main = document.querySelector('main');
 
     let userGameboard = document.querySelector('.user-tile-cont');
-    let compGameboard = document.querySelector('.comp-tile-cont');
+    let userGameboardColLabel = document.querySelector('.user-gameboard-col-label');
+    let userGameboardRowLabel = document.querySelector('.user-gameboard-row-label');
 
+    let compGameboard = document.querySelector('.comp-tile-cont');
+    let compGameboardColLabel = document.querySelector('.comp-gameboard-col-label');
+    let compGameboardRowLabel = document.querySelector('.comp-gameboard-row-label');
+    
     // render gameboard tiles
 
     for (let i = 97; i <= 106; i++) {
@@ -26,6 +30,35 @@ const renderDOM = () => {
             compGameboard.appendChild(newTile2);
         }
     }
+
+    // render the gameboard row labels
+
+    for (let i = 97; i <= 106; i++) {
+        let newLabel1 = document.createElement('div');
+        newLabel1.innerText = `${String.fromCharCode(i)}`;
+        newLabel1.classList.add('tile-label');
+        userGameboardRowLabel.appendChild(newLabel1);
+
+        let newLabel2 = document.createElement('div');
+        newLabel2.innerText = `${String.fromCharCode(i)}`;
+        newLabel2.classList.add('tile-label');
+        compGameboardRowLabel.appendChild(newLabel2);
+    }
+
+    // render the gameboard column labels
+
+    for (let i = 1; i <= 10; i++) {
+        let newLabel1 = document.createElement('div');
+        newLabel1.innerText = i;
+        newLabel1.classList.add('tile-label');
+        userGameboardColLabel.appendChild(newLabel1);
+
+        let newLabel2 = document.createElement('div');
+        newLabel2.innerText = i;
+        newLabel2.classList.add('tile-label');
+        compGameboardColLabel.appendChild(newLabel2);
+    }
+
 }
 
 export { renderDOM }
