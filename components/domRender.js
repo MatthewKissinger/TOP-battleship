aa68d6914b917b88e2d31ca5cf7c6dd4f3d1af2f
Matt <matthew.t.kissinger@gmail.com>
2022-08-08 15:38:52 -0700

    rendering the gameboards

--- components/domRender.js
+++ components/domRender.js
@@ -9,6 +9,23 @@ const renderDOM = () => {
     let messageDisplay = document.querySelector('.message-display');
     let startBtn = document.querySelector('.game-start');
     let main = document.querySelector('main');
+
+    let userGameboard = document.querySelector('.user-tile-cont');
+    let compGameboard = document.querySelector('.comp-tile-cont');
+
+    // render gameboard tiles
+
+    for (let i = 97; i <= 106; i++) {
+        for (let j = 1; j <= 10; j++) {
+            let newTile1 = document.createElement('div');
+            newTile1.classList.add('user-tile', `${String.fromCharCode(i)}${j}`);
+            userGameboard.appendChild(newTile1);
+
+            let newTile2 = document.createElement('div');
+            newTile2.classList.add('comp-tile', `${String.fromCharCode(i)}${j}`);
+            compGameboard.appendChild(newTile2);
+        }
+    }
 }
 
 export { renderDOM }
