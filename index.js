93586de1236713d28cf5a790063374da750f378a
Matt <matthew.t.kissinger@gmail.com>
2022-09-28 14:06:28 -0700

    github pages is tested and active

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // TO DO 
 
+// update the styling 
+// add a header and footer
+// give it some pazazz
+
 import { renderDOM } from "./components/domRender.js";
 import { playGame } from "./components/playGame.js"
 
